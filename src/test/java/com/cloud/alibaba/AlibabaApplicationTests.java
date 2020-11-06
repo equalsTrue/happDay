74d2422c30ac9b388bc0a5170928b417d3f03a4e
David <david.li@starpavilion-digital.com>
2020-11-06 18:37:03 +0800

    create new project

---
+++ src/test/java/com/cloud/alibaba/AlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.alibaba;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
