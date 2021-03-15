92fae61a3652360e907ec475897b8df5fbcd4941
David <david.li@starpavilion-digital.com>
2021-03-15 19:55:51 +0800

    update

--- spring-cloud-hystrix/src/main/java/com/david/springcloudhystrix/SpringCloudHystrixApplication.java
+++ spring-cloud-hystrix/src/main/java/com/david/springcloudhystrix/SpringCloudHystrixApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @EnableFeignClients
+@EnableZuulProxy
 public class SpringCloudHystrixApplication {
 
     public static void main(String[] args) {
