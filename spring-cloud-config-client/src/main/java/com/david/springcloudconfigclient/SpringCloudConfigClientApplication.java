cee4d56c5d8b394c63774a585d8686440e622e77
David <david.li@starpavilion-digital.com>
2021-03-22 17:35:09 +0800

    update

--- spring-cloud-config-client/src/main/java/com/david/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/david/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigClientApplication.class, args);
     }
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.application.name}")
     String name;
 
 
